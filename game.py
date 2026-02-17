de8ce18edbf5cd86d861e260b90d246147f1301a
ayman722 <aymanknights@gmail.com>
2026-02-16 21:30:44 -0600

    Add number guessing game implementation

---
+++ game.py
@@ -0,0 +1,26 @@
++ import random
++
++ def guessing_game():
++    number = random.randint(1,100)
++    attempts = 0
++
++    print(" Welcome to the Number Guessing Game!")
++    print(" I'm thinking of a number between 1 and 100.")
++
++    while True:
++        try:
++ guess = int(input(Enter your guess: "))
++ attempts =+ 1
++
++ if guess < number:
++    print("Too low! Try again,")
++    elif guess > number:
++ print("Too high! Try again,")
++ else:
++ print(f"Congratulations! You got it in {attempts} attempts!")
++ break
++ except ValueError:
+         print("Please enter a valid number!")
++
++   if __name__ ==  "__main__":
++        guessing_game()
